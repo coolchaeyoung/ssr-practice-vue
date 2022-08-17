433fc599a7ecb3bd41959dc0ecb8282222640c3c
coolchaeyoug <ggodchae@naver.com>
2022-08-17 18:48:27 +0900

    feat: SSR 설정(일부)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import { createApp } from "vue";
+import { createApp, createSSRApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-const app = createApp(App);
-app.use(store).use(router).mount("#app");
+export default () => {
+  const isSSR = process.env.VUE_APP_SSR;
+
+  const app = (isSSR ? createSSRApp : createApp)(App);
+  app.use(store).use(router);
+
+  return {
+    app,
+    router,
+    store,
+  };
+};
